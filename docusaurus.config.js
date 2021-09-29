8378211cbe264bfdb235ba47e417a8773c5ecf40
Maximilian Alexander <max@ditto.live>
2021-09-28 21:27:10 -0700

    adding gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,12 @@ module.exports = {
   favicon: "img/favicon.ico/favicon.ico", 
   organizationName: "getditto", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
+  plugins: ['@docusaurus/plugin-google-gtag'],
   themeConfig: {
+    gtag: {
+      // You can also use your "G-" Measurement ID here.
+      trackingID: 'G-0PDX043H6Z',
+    },
     navbar: {
       title: "Ditto",
       logo: {
