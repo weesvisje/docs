b70bca50056cfb025495566f92e2115f315570f1
Max Alexander <max@ditto.live>
2021-11-09 16:40:42 -0800

    adding latest version

--- src/components/Changelog.tsx
+++ src/components/Changelog.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import semver from "semver";
-import { stripIndent } from "common-tags";
+import dedent from "ts-dedent";
 import moment from "moment";
 import markdownIt from "markdown-it";
+import CodeBlock from "../theme/CodeBlock";
 
 export interface SDKInfo {
   framework: string;
@@ -21,11 +22,11 @@ export interface SDKInfo {
   languageOrPlatform?: string;
 }
 
-const downloadedFrameworks: { [key: string]: any } = parseFrameworks(
+const downloadedFrameworks: { [key: string]: SDKInfo[] } = parseFrameworks(
   require("../../ditto-changelog.json") || {}
 );
 
-function parseFrameworks(dittoChangeLog: { [key: string]: any }): {
+export function parseFrameworks(dittoChangeLog: { [key: string]: any }): {
   [framework: string]: SDKInfo[];
 } {
   const md = markdownIt();
@@ -72,7 +73,7 @@ function parseFrameworks(dittoChangeLog: { [key: string]: any }): {
               description: md.render(body.description), // description is markdown, we need to convert it to HTML
             };
             if (framework === "cocoa") {
-              sdkInfo["installationSnippet"] = md.render(stripIndent`
+              sdkInfo["installationSnippet"] = md.render(dedent`
               ~~~ruby
               pod '${frameworkName}', '=${version}'
               ~~~
@@ -88,7 +89,7 @@ function parseFrameworks(dittoChangeLog: { [key: string]: any }): {
             }
             if (framework === "android") {
               const androidName = frameworkName.toLowerCase();
-              sdkInfo["installationSnippet"] = md.render(stripIndent`
+              sdkInfo["installationSnippet"] = md.render(dedent`
               ~~~html
               implementation "live.ditto:${androidName}:${version}"
               ~~~`);
@@ -102,7 +103,7 @@ function parseFrameworks(dittoChangeLog: { [key: string]: any }): {
             if (framework === "dotnet") {
               sdkInfo["friendlyName"] = ".NET";
               sdkInfo["friendlyDescription"] = ".NET Standard 2.1";
-              sdkInfo["installationSnippet"] = md.render(stripIndent`
+              sdkInfo["installationSnippet"] = md.render(dedent`
               Command Line
               ~~~html
               Install-Package Ditto -Version ${version}
@@ -117,7 +118,7 @@ function parseFrameworks(dittoChangeLog: { [key: string]: any }): {
             if (framework === "js") {
               sdkInfo["friendlyName"] = "JS";
               sdkInfo["friendlyDescription"] = "Web only";
-              sdkInfo["installationSnippet"] = md.render(stripIndent`
+              sdkInfo["installationSnippet"] = md.render(dedent`
               ~~~shell
               npm install --save @dittolive/ditto@${version}
               ~~~
@@ -179,52 +180,125 @@ export default function Changelog() {
 
   return (
     <div className="prose prose-lg max-w-none">
-        <div className="col margin-vert--lg" style={{ maxWidth: "800px" }}>
-          <Tabs
-            defaultValue="swift"
-            groupId="framework"
-            values={[
-              { label: "JavaScript", value: "javascript" },
-              { label: "Swift", value: "swift" },
-              { label: "Obj-C", value: "objc" },
-              { label: "Kotlin", value: "kotlin" },
-              { label: "Java", value: "java" },
-              { label: "C#", value: "csharp" },
-              { label: "C++", value: "cpp" },
-            ]}
-          >
-            <TabItem value="javascript">
-              <TabContents title="JavaScript" sdkInfos={frameworks["js"]} />
-            </TabItem>
-            <TabItem value="swift">
-              <TabContents title="DittoSwift" sdkInfos={frameworks["cocoa"]} />
-            </TabItem>
-            <TabItem value="objc">
-              <TabContents title="DittoObjC" sdkInfos={frameworks["cocoa"]} />
-            </TabItem>
-            <TabItem value="java">
-              <TabContents
-                title="Java Android"
-                sdkInfos={frameworks["android"]}
-              />
-            </TabItem>
-            <TabItem value="kotlin">
-              <TabContents
-                title="Kotlin Android"
-                sdkInfos={frameworks["android"]}
-              />
-            </TabItem>
-            <TabItem value="csharp">
-              <TabContents title="C# / .NET" sdkInfos={frameworks["dotnet"]} />
-            </TabItem>
-            <TabItem value="cpp">
-              <TabContents
-                title="C++ Linux / iOS"
-                sdkInfos={frameworks["cpp"]}
-              />
-            </TabItem>
-          </Tabs>
-        </div>
+      <div className="col margin-vert--lg" style={{ maxWidth: "800px" }}>
+        <Tabs
+          defaultValue="swift"
+          groupId="framework"
+          values={[
+            { label: "JavaScript", value: "javascript" },
+            { label: "Swift", value: "swift" },
+            { label: "Obj-C", value: "objc" },
+            { label: "Kotlin", value: "kotlin" },
+            { label: "Java", value: "java" },
+            { label: "C#", value: "csharp" },
+            { label: "C++", value: "cpp" },
+          ]}
+        >
+          <TabItem value="javascript">
+            <TabContents title="JavaScript" sdkInfos={frameworks["js"]} />
+          </TabItem>
+          <TabItem value="swift">
+            <TabContents title="DittoSwift" sdkInfos={frameworks["cocoa"]} />
+          </TabItem>
+          <TabItem value="objc">
+            <TabContents title="DittoObjC" sdkInfos={frameworks["cocoa"]} />
+          </TabItem>
+          <TabItem value="java">
+            <TabContents
+              title="Java Android"
+              sdkInfos={frameworks["android"]}
+            />
+          </TabItem>
+          <TabItem value="kotlin">
+            <TabContents
+              title="Kotlin Android"
+              sdkInfos={frameworks["android"]}
+            />
+          </TabItem>
+          <TabItem value="csharp">
+            <TabContents title="C# / .NET" sdkInfos={frameworks["dotnet"]} />
+          </TabItem>
+          <TabItem value="cpp">
+            <TabContents title="C++ Linux / iOS" sdkInfos={frameworks["cpp"]} />
+          </TabItem>
+        </Tabs>
+      </div>
     </div>
   );
+}
+
+interface InstallCodeProps {
+  framework: string;
+  variant?: string;
+}
+
+export function InstallCode({
+  framework,
+  variant,
+}: InstallCodeProps): ReactElement {
+  const latest = downloadedFrameworks[framework][0];
+  let installCode: string = "";
+
+  switch (framework) {
+    case "js":
+      installCode = dedent`
+        # using npm
+        npm install @dittolive/ditto@${latest.version} --save
+
+        # using yarn
+        yarn add @dittolive/ditto@${latest.version}
+      `;
+      break;
+    case "cocoa":
+      if (variant === "swift") {
+        installCode = dedent`
+        pod 'DittoSwift', '=${latest.version}'
+      `;
+      }
+
+      if (variant === "objc") {
+        installCode = dedent`
+        pod 'DittoObjC', '=${latest.version}'
+      `;
+      }
+
+      break;
+    case "android":
+      installCode = dedent`
+      dependencies {
+        // ...
+        implementation "live.ditto:ditto:${latest.version}"
+      }
+        
+      android {
+        // ...
+  
+        compileOptions {
+            sourceCompatibility JavaVersion.VERSION_1_8
+            targetCompatibility JavaVersion.VERSION_1_8
+        }
+      }
+      `;
+    case "dotnet":
+
+      if (variant === 'package-manager') {
+        installCode = dedent`Install-Package Ditto -Version ${latest.version}`;
+      }
+      
+      if (variant === 'cli') {
+        installCode = dedent`dotnet add package Ditto --version ${latest.version}`;
+      }
+
+      if (variant === 'package-reference') {
+        installCode = dedent`<PackageReference Include="Ditto" Version="${latest.version}" />`;
+      }
+
+      break
+    case "cpp":
+      installCode = `curl -O https://software.ditto.live/cpp-${variant}/Ditto/${latest.version}/dist/Ditto.tar.gz && tar xvfz Ditto.tar.gz`
+    default:
+      break;
+  }
+
+  return <CodeBlock>{installCode}</CodeBlock>;
 }
